91a5878b9d626cd2d86f690d123f0816f14bb819
monicaIs <monik19537@gmail.com>
2022-07-17 10:32:24 -0600

    Se agregaron más detalles al formulario del lugar

--- app/src/main/java/com/example/easybookingapp/lugar.java
+++ app/src/main/java/com/example/easybookingapp/lugar.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.widget.ImageView;
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Spinner;
 
 
 import com.squareup.picasso.Picasso;
 
 public class lugar extends AppCompatActivity {
 
-    ImageView imagenUser, imagenTerraza;
+    ImageView imagenUser, imagenTerraza,imagenHome,imagenSearch,imagenSave,imagenNoti;
+    Spinner cambiarImagen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +33,25 @@ public class lugar extends AppCompatActivity {
                 .load("https://i.ibb.co/Qn0L6ph/restaurant-Terraz.png")
                 .into(imagenTerraza);
 
+        imagenHome = findViewById(R.id.homeLugar);
+        Picasso.with(this)
+                .load("https://i.ibb.co/wRDhYCg/hogar.png")
+                .into(imagenHome);
+
+        imagenSearch = findViewById(R.id.buscarLugar);
+        Picasso.with(this)
+                .load("https://i.ibb.co/jMwm6CK/search.png")
+                .into(imagenSearch);
+
+        imagenSave = findViewById(R.id.guardarLugar);
+        Picasso.with(this)
+                .load("https://i.ibb.co/W5cn0bj/save.png")
+                .into(imagenSave);
+
+        imagenNoti = findViewById(R.id.notiLugar);
+        Picasso.with(this)
+                .load("https://i.ibb.co/pK63PH4/bell.png")
+                .into(imagenNoti);
 
     }
 }
