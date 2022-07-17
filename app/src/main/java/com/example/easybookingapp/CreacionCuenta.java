c4f37cd3e3e2bafc3bbbe201ff77f17c7fb261df
Catherine V <32275945+Catherine1998@users.noreply.github.com>
2022-07-16 21:32:32 -0600

    Pagina creacion cuenta

---
+++ app/src/main/java/com/example/easybookingapp/CreacionCuenta.java
@@ -0,0 +1,14 @@
+package com.example.easybookingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreacionCuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creacion_cuenta);
+    }
+}
