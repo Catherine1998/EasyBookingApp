611c9bed589ea63a8b0e48b95877691a4d9f37d0
monicaIs <monik19537@gmail.com>
2022-07-17 12:53:38 -0600

    Se agregaron imagenes dinámicas y se compuso la barra de inicio

--- app/src/main/java/com/example/easybookingapp/lugar.java
+++ app/src/main/java/com/example/easybookingapp/lugar.java
@@ -10,28 +10,73 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 
 import com.squareup.picasso.Picasso;
 
 public class lugar extends AppCompatActivity {
 
-    ImageView imagenUser, imagenTerraza,imagenHome,imagenSearch,imagenSave,imagenNoti;
+    ImageView imagenUser, foto,imagenHome,imagenSearch,imagenSave,imagenNoti;
     Spinner cambiarImagen;
+    int imagen = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lugar);
 
+        cambiarImagen = (Spinner) findViewById(R.id.spinner);
         imagenUser = findViewById(R.id.usuario);
+        foto = findViewById(R.id.terraza);
+
         Picasso.with(this)
                 .load("https://i.ibb.co/P6BB0Md/user.png")
                 .into(imagenUser);
 
-        imagenTerraza = findViewById(R.id.terraza);
-        Picasso.with(this)
-                .load("https://i.ibb.co/Qn0L6ph/restaurant-Terraz.png")
-                .into(imagenTerraza);
+        cambiarImagen.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+                imagen = parent.getSelectedItemPosition();
+                switch(imagen) {
+                    case 0:
+                        Picasso.with(parent.getContext())
+                                .load("https://i.ibb.co/R7VfBrT/imagen.png")
+                                .into(foto);
+                        break;
+                    case 1:
+                        Picasso.with(parent.getContext())
+                                .load("https://i.ibb.co/Qn0L6ph/restaurant-Terraz.png")
+                                .into(foto);
+                        break;
+                    case 2:
+                        Picasso.with(parent.getContext())
+                                .load("https://i.ibb.co/cyK0b00/jardin.jpg")
+                                .into(foto);
+                        break;
+                    case 3:
+                        Picasso.with(parent.getContext())
+                                .load("https://i.ibb.co/J7GQfFL/posada-rodrigo-planta-baja.jpg")
+                                .into(foto);
+                        break;
+                    case 4:
+                        Picasso.with(parent.getContext())
+                                .load("https://i.ibb.co/wBwprcc/planta-alta.jpg")
+                                .into(foto);
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent){
+                Picasso.with(parent.getContext())
+                        .load("https://i.ibb.co/R7VfBrT/imagen.png")
+                        .into(foto);
+            }
+        });
+
+            Picasso.with(this)
+                    .load("https://i.ibb.co/R7VfBrT/imagen.png")
+                    .into(foto);
+
 
         imagenHome = findViewById(R.id.homeLugar);
         Picasso.with(this)
