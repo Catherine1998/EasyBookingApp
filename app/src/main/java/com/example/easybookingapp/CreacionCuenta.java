a5c9a5102cd2eb44b6a3608d67a74c5d76d008b1
Catherine V <32275945+Catherine1998@users.noreply.github.com>
2022-07-25 17:17:55 -0600

    Datepicker

--- app/src/main/java/com/example/easybookingapp/CreacionCuenta.java
+++ app/src/main/java/com/example/easybookingapp/CreacionCuenta.java
@@ -2,16 +2,25 @@ package com.example.easybookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class CreacionCuenta extends AppCompatActivity {
 
     ImageView imageViewNombre, imageViewApellido, imageViewDate, imageViewCorreo, imageViewContra, imageViewConfiContra;
+    EditText fechaCampo;
+    Calendar calendario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,15 +58,42 @@ public class CreacionCuenta extends AppCompatActivity {
                 .load("https://i.ibb.co/C90h8Hf/llave.png")
                 .into(imageViewConfiContra);
 
+        fechaCampo = findViewById(R.id.txtfecha);
+        Calendar calendario = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener fecha = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                calendario.set(Calendar.YEAR, year);
+                calendario.set(Calendar.MONTH, month);
+                calendario.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateCalendar();
+            }
+
+            private void updateCalendar() {
+                String Format = "dd/MM/yy";
+                SimpleDateFormat sdf = new SimpleDateFormat(Format, Locale.US);
+                fechaCampo.setText(sdf.format(calendario.getTime()));
+            }
+        };
+        fechaCampo.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+                new DatePickerDialog(CreacionCuenta.this, fecha, calendario.get(Calendar.YEAR), calendario.get(Calendar.MONTH), calendario.get(Calendar.DAY_OF_MONTH))
+                        .show();
+
+            }
+        });
 
     }
 
-    public void IrInicio(View view){
+
+    public void IrInicio(View view) {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
     }
 
-    public void IrIniciarSession(View view){
+    public void IrIniciarSession(View view) {
         Intent u = new Intent(this, IniciarSesion.class);
         startActivity(u);
     }
